1ea23696a21d4ed867cb41fa97ec46806ead1e7b
Adam Camilli <a.o.camilli25@gmail.com>
2017-02-20 17:59:31 -0500

    Finalized map() for Phase 1 (I hope)

--- mem_manager.c
+++ mem_manager.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 
-#define SIZE 63
+#define SIZE 64
 #define PAGE_LIMIT 4
 #define SEED 4
 
@@ -21,6 +21,8 @@ typedef struct instruction {
 
 char memory[SIZE];
 page **free_list;
+char page_table[SIZE / PAGE_LIMIT];
+int used_pids[PAGE_LIMIT] = {-1,-1,-1,-1};
 
 int map(instruction instruc);
 
@@ -35,11 +37,14 @@ int main (int argc, char *argv[]) {
 
   free_list = malloc(sizeof(page*)*4);
   
-  free_list[0] = &pg1;
+  free_list[0] = &pg1; // Reserved for page table
   free_list[1] = &pg2;
   free_list[2] = &pg3;
   free_list[3] = &pg4;
 
+  instruction page_table_map = {.pid = 0, .type="map", .v_addr = 0, .value = 1};
+  map(page_table_map);
+  
   // Instruction buffer 
   char buf[100];
   // Instruction prompter
@@ -89,41 +94,53 @@ int map(instruction instruc) {
     return 1;
   }
 
+  // Check if pid is already in use
+  int i = 0;
+  for (; i < PAGE_LIMIT; i++) {
+    if (instruc.pid == used_pids[i]) {
+      printf("PID %d is already in use!\n",instruc.pid);
+      return 1;
+    }
+  }
+  
   // Try to locate two empty pages: If they cannot be found, we don't have enough to map
-  int i;
   int j = 0;
   int pg_process = -1;
-  int pg_pgtable = -1;
   
   for (i = 0; i < 4; i++) {
     //printf("freelist: %i", free_list[i]->start);
     if (free_list[i] != NULL) {
       j++;
-      //printf("j: %i", j);
-      if (pg_pgtable < 0) 
-	pg_pgtable = i;
-      else if (pg_process < 0 && i != pg_pgtable) 
-	pg_process = i; 
+      //printf("j: %i", j); 
+      pg_process = i;
+      break;
     }
   }
-  if (j < 2) {
+  if (j < 1) {
     printf("Not enough pages left!\n");
     return 1;
   }
-  
-  char page_table[16];
-  int page_num1 = free_list[pg_pgtable]->frame;
-  int page_num2 = free_list[pg_process]->frame;
-  page_table[0] = page_num1;
-  printf("Put page table for PID %d into physical frame %d.\n", instruc.pid, page_num1);
-  page_table[1] = page_num2;
-  printf("Mapped virtual address %d (page %d) into physical frame %d.\n", instruc.v_addr, 0, page_num2); 
 
-  for(i = free_list[pg_pgtable]->start; i < free_list[pg_pgtable]->offset; i++) {
-    memory[i] = page_table[i];
+  used_pids[i] = instruc.pid; // Add PID to list of PIDS in use
+  
+  int frame = free_list[pg_process]->frame;
+  if (frame == 0)
+    printf("Mapped page table into physical frame %d.\n", frame); // Frame 0 is reserved for page table
+  else 
+    printf("Mapped virtual address %d (page %d) into physical frame %d.\n", instruc.v_addr, pg_process, frame);
+
+  // Put information in page table
+  // Usage of frame is accurate for page_table indices because pages are allocated to processes in ascending order (FIFO)
+  if (frame > 0) { // We don't care about info regarding page table itself
+    char info_byte = instruc.pid + frame + instruc.v_addr + instruc.value;
+    page_table[frame-1] = info_byte; 
+    printf("Placed info byte %d into %d on page table\n", info_byte, frame-1);
+    for(i = free_list[frame]->start; i < free_list[frame]->offset; i++) {
+      memory[i] = page_table[i];
+    }
   }
-  free_list[pg_pgtable] = NULL; // Mark page as used
-  free_list[pg_process] = NULL; // Mark page as used
+  
+  free_list[frame] = NULL; // Mark page as used
 
   return 0;
 }
