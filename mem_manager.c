205567188119126e46a28d18063e2cc1d71bb7c1
Adam Camilli <a.o.camilli25@gmail.com>
2017-02-24 22:15:52 -0500

    Part 2 sorta maybe working

--- mem_manager.c
+++ mem_manager.c
@@ -24,14 +24,19 @@ typedef struct instruction {
   unsigned int value;   // [0-255]
 } instruction;
 
+// Global variables and structures
+
 unsigned char memory[SIZE];
 unsigned char disk[SIZE*SIZE];
 FILE* swap_space;
-int free_list[4];
-int mem_registry[MAX_PROCESSES];
-int disk_registry[MAX_PROCESSES];
+
+int free_list[PAGE_LIMIT];
+int disk_free[PAGE_LIMIT * SIZE];
+int page_table_in_memory = 1;
 // Which page to evict in the event of a swap, follows a round-robin strategy
-int next_evict = 0;
+int next_evict = 1;
+
+// Function headers
 
 int map(instruction instruc);
 int store(instruction instruc);
@@ -40,31 +45,26 @@ int load(instruction instruc);
  * at from offset to a swap file, and from there to 
  * page in disk/memory starting at to offset
  */
-int swap(pte entry, int to_disk);
+int swap(pte *entry, int to_disk);
 
 int main (int argc, char *argv[]) {
 
   // Place offsets into free list
-  free_list[0] = -1; // Reserved for page table
+  free_list[0] = -1; // Initially reserved for page table
   free_list[1] = 16;
   free_list[2] = 32;
   free_list[3] = 48;
 
-  // Mark all registers as unused except the one for our page table
-  for (int i = 0; i < MAX_PROCESSES; i++) { 
-    if (!i) mem_registry[i] = 0;
-    else mem_registry[i] = -1;
-    disk_registry[i] = -1;
-  } 
+  for (int i = 0; i < PAGE_LIMIT * PAGE_LIMIT; i++)
+    disk_free[i] = 16*i;
 
   // Open swap file
-  swap_space = fopen("swap_space.txt","w+");
+  swap_space = fopen("swap_space.txt","wb+");
   
   // Instruction buffer
-  int loop = 1;
   char buf[100];
   // Instruction prompter
-  while(loop) {
+  while(1) {
     printf("Instruction: ");
     fgets(buf, sizeof buf, stdin);
 
@@ -76,11 +76,31 @@ int main (int argc, char *argv[]) {
     for (tok = strtok(buf,", "); tok != NULL; tok = strtok(NULL," ,")) {
       buf_chunks[i] = tok;
       i++;
-    } if (i != 4) { // Exactly 4 arguments must be passed in
-      printf("Please enter exactly 4 arguments for instruction. You entered %d\n",i);
+    } if (i != 4 && strcmp(buf_chunks[0],"end") && strcmp(buf_chunks[0],"status")) {
+      printf("Please enter exactly 4 arguments for instruction. You entered %d\n\n",i);
       continue;
+    } if (!strcmp(buf_chunks[0],"end")) {
+      printf("Goodbye!\n");
+      break; // Exit while loop
+    } else if (!strcmp(buf_chunks[0], "status")) {
+      printf("\n\tMemory: \n");
+      for (int i = 0; i < PAGE_LIMIT; i++) {
+	printf("Page %d:   ",i);
+	for (int j = 16*i; j < 16*(i+1); j++)
+	  printf("%d ",memory[j]);
+	printf("\n");
+      }
+      printf("\n\tDisk: \n");
+      for (int i = 0; i < PAGE_LIMIT; i++) {
+	printf("Sector %d:  ",i);
+	for (int j = 16*i; j < 16*(i+1); j++)
+	  printf("%d ", disk[j]);
+	printf("\n");
+      }
+      printf("\n");
+      continue; // Ask for next instruction
     }
-
+    
     // Populate struct with converted tokens
     instruction instruc;
     instruc.pid = (int) strtol(buf_chunks[0], (char **)NULL, 10);
@@ -88,18 +108,21 @@ int main (int argc, char *argv[]) {
     instruc.v_addr = (unsigned int) strtol(buf_chunks[2], (char **)NULL, 10);
     instruc.value = (unsigned int) strtol(buf_chunks[3], (char **)NULL, 10);
 
+    if (instruc.pid > 3 || instruc.pid < 0 || instruc.v_addr > 63 || instruc.v_addr < 0) {
+      printf("PID and virtual address must be greater than 0 and less than 3 and 65, respectively\n");
+      continue;
+    }
+
     if (!strcmp(instruc.type, "map")) {
       map(instruc);
     } else if (!strcmp(instruc.type, "store")) {
       store(instruc);
     } else if (!strcmp(instruc.type, "load")) {
       load(instruc);
-    } else if (!strcmp(instruc.type, "end")) {
-      printf("Goodbye!\n");
-      loop = 0;
     } else {
       printf("\"%s\" is not a valid instruction.\n",instruc.type);
     }
+    printf("\n");
   }
 
   fclose(swap_space);
@@ -117,32 +140,37 @@ int map(instruction instruc) {
   }
   
   // Check if virtual address has already been mapped for this process
+  // And if whether it currently resides in memory or disk
   instruc.v_addr = (instruc.v_addr / 16) * 16; // Convert virtual address to appropriate multiple of 16
   for (int i = 0; i < 16; i++) {
-    if (memory[i]) { // Will be non zero if containing a page table entry struct
-      pte* entry = (pte*) &memory[i];
-      if (entry->pid == instruc.pid && entry->vpn == instruc.v_addr / 16) {
-	printf("Virtual address %d has is already in physical frame %d for process %d\n", entry->vpn, entry->pfn, entry->pid);
+    if (memory[i]) {  // Will be non zero if containing a page table entry struct
+      pte* temp = (pte*) &memory[i];
+      if (temp->pid == instruc.pid && temp->vpn == instruc.v_addr / 16) {
+	printf("Virtual page %d is already mapped to this process\n",temp->vpn);
 	return -1;
-      } else if (entry->pid == instruc.pid && entry->pfn)
+      } else if (temp->pid == instruc.pid) 
 	first_map = 0;
     }
   }
-
+  
   pte our_entry = {.io = 1, .pid = instruc.pid, .rw = instruc.value}; // Page table entry for this map
 
-  int j;
-  for (j = 0; j < MAX_PROCESSES; j++) {
+  int found = 0;
+  for (int j = 0; j < MAX_PROCESSES; j++) {
     if (free_list[j] > -1) {
       our_entry.vpn = instruc.v_addr / 16;
       our_entry.pfn = j;
+      found++;
       break;
     }
-  } if (j == 4) {
-    printf("Not enough pages left!\n");
-    return -1;
+  } if (!found) {
+    for (int i = 0; i < MAX_PROCESSES; i++)
+      if (disk_free[i] > -1) {
+	swap( ((pte*) &memory[next_evict-1]), disk_free[i]);
+	break;
+      }
+    return map(instruc);
   }
-
 
   if (first_map)
     printf("Placed page table for PID %d into frame 0\n",instruc.pid);
@@ -158,7 +186,6 @@ int map(instruction instruc) {
     }
   }
 
-  mem_registry[instruc.pid] = free_list[our_entry.pfn];
   free_list[our_entry.pfn] = -1;
     
   return 0;
@@ -172,15 +199,19 @@ int store(instruction instruc) {
     return 1;
   }
 
+  int in_memory;
+  
   // Now try to find corresponding page table entry for this PID and virtual address
-  pte our_entry;
+  // and if it currently in disk or memory
+  pte* our_entry;
   int entry_index = -1;
   for (int i = 0; i < 16; i++) {
     if (memory[i]) {
       pte* entry = (pte*) &memory[i];
       if (entry->pid == instruc.pid && entry->vpn == instruc.v_addr / 16) {
 	entry_index = i;
-	our_entry = *entry;
+	our_entry = entry;
+	in_memory = our_entry->io;
 	break;
       }
     }
@@ -189,12 +220,42 @@ int store(instruction instruc) {
   if (entry_index == -1) {
     printf("Virtual address %d has not been mapped to process %d\n", instruc.v_addr, instruc.pid);
     return -1;
-  } else if (!our_entry.rw) {
-    printf("Page %d of process %d is not writeable\n", our_entry.vpn, instruc.pid);
+  } else if (!our_entry->rw) {
+    printf("Writes are not allowed to this page\n");
     return -1;
   }
+
+  // If not in memory, swap it in
+  if (!in_memory) {
+    int in_use = 0;
+    for (int i = 0; i < 16; i++) {
+      if (memory[i]) {  // Will be non zero if containing a page table entry struct
+	pte* temp = (pte*) &memory[i];
+	if (temp->pfn == next_evict) {
+	  in_use = 1;
+	  break;
+	}
+      }
+    }
+    // If next page in evict order is in use, swap it first so it
+    // is not overwritten
+    if (in_use) {
+      pte* old_page = (pte*) &memory[next_evict];
+      int free_disk_offset;
+
+      for (int i = 0; i < PAGE_LIMIT * PAGE_LIMIT; i++)
+	if (disk_free[i] > -1) {
+	  free_disk_offset = disk_free[i];
+	  break;
+	}
+      
+      swap(old_page, free_disk_offset);
+    }
+    swap(our_entry, free_list[next_evict]);
+    return store(instruc);
+  }
   
-  int p_addr = (16 * our_entry.pfn) + (instruc.v_addr % 16); // Perform address translation
+  int p_addr = (16 * our_entry->pfn) + (instruc.v_addr % 16); // Perform address translation
   memory[p_addr] = instruc.value;
   printf("Stored value %d at virtual address %d (physical address %d)\n",instruc.value,instruc.v_addr,p_addr);
 
@@ -203,15 +264,18 @@ int store(instruction instruc) {
 
 int load(instruction instruc) {
 
+  int in_memory = 1;
+  
   // Try to find corresponding page table entry for this PID and virtual address
-  pte our_entry;
+  pte* our_entry;
   int entry_index = -1;
   for (int i = 0; i < 16; i++) {
     if (memory[i]) {
       pte* entry = (pte*) &memory[i];
       if (entry->pid == instruc.pid && entry->vpn == instruc.v_addr / 16) {
 	entry_index = i;
-	our_entry = *entry;
+	our_entry = entry;
+	in_memory = our_entry->io;
 	break;
       }
     }
@@ -220,9 +284,39 @@ int load(instruction instruc) {
   if (entry_index == -1) {
     printf("Virtual address %d has not been mapped to process %d\n", instruc.v_addr, instruc.pid);
     return -1;
-  } 
+  }
 
-  int p_addr = (16 * our_entry.pfn) + (instruc.v_addr % 16); // Perform address translation
+    // If not in memory, swap it in
+  if (!in_memory) {
+    int in_use = 0;
+    for (int i = 0; i < 16; i++) {
+      if (memory[i]) {  // Will be non zero if containing a page table entry struct
+	pte* temp = (pte*) &memory[i];
+	if (temp->pfn == next_evict) {
+	  in_use = 1;
+	  break;
+	}
+      }
+    }
+    // If next page in evict order is in use, swap it first so it
+    // is not overwritten
+    if (in_use) {
+      pte* old_page = (pte*) &memory[next_evict];
+      int free_disk_offset;
+
+      for (int i = 0; i < PAGE_LIMIT * PAGE_LIMIT; i++)
+	if (disk_free[i] > -1) {
+	  free_disk_offset = disk_free[i];
+	  break;
+	}
+      
+      swap(old_page, free_disk_offset);
+    }
+    swap(our_entry, free_list[next_evict]);
+    return load(instruc);
+  }
+
+  int p_addr = (16 * our_entry->pfn) + (instruc.v_addr % 16); // Perform address translation
 
   if (memory[p_addr] == 0) {
     printf("0 found at virtual address %d (physical address %d): Could be empty or 0...\n", instruc.v_addr, p_addr);
@@ -233,105 +327,65 @@ int load(instruction instruc) {
   return 0;
 }
 
-/*
-int swap(pte *entry, int to_disk) { 
-
-  // Ensure entry is actually in page table
-  int entry_index = -1;
-  for (int i = 0; i < 16; i++) {
-    if (memory[i]) {
-      pte* temp = (pte*) &memory[i];
-      if (temp->pid == entry->pid && temp->vpn == entry->vpn) {
-	entry_index = i;
-        break;
-      }
-    }
-  } if (entry_index == -1) {
-    printf("Tried to swap nonexistent page\n");
-    return -1;
-  }  
+int swap(pte *entry, int new_offset) { 
   
-  // Clear appropriate registers and (if memory) mark as unused in free_list
-  int 
-  if (to_disk)
-    
+  // Mark as free lists unused
+  int prev_offset = entry->pfn * 16;
+  int to_disk = entry->io;
   
   if (to_disk) {
-    free_list[entry->pid] = 16 * entry->pfn;
-    mem_registry[entry->pid] = -1;
+    free_list[entry->pfn] = 16 * entry->pfn;
   } else {
-    disk_registry[entry->pid] = -1;
+    disk_free[entry->pfn] = 16 * entry->pfn;
   }
   
-  // Append page contents from memory/disk to end of swap file
-  int start = 
-  int file_start = (int) ftell(swap_space);
-
-  while (from < start + 16) {
+  // Append page contents from memory/disk to swap file
+  printf("At position %d in file\n", fseek(swap_space, 0, SEEK_SET));
+  
+  for (int i = prev_offset; i < prev_offset + 16; i++) {
     if (to_disk) {
-      if (is_page_table && from == start + 2)  // If a page table, make sure to change PFN index, as it is no longer accurate
-	memory[from] = to / 16;
-      printf("%ld %d\n", ftell(swap_space), fputc(memory[from++], swap_space));
-      memory[from - 1] = 0; // After copying, erase in memory
+      fputc(memory[i], swap_space);
+      memory[i] = 0; // After copying, erase in memory
     } else {
-      if (is_page_table && from == start + 2) // If a page table, make sure to change PFN index, as it is no longer accurate
-	disk[from] = to / 16;
-      fputc(disk[from++], swap_space);
-      disk[from - 1] = 0; // After copying, erase in disk
+      fputc(disk[i], swap_space);
+      disk[i] = 0; // After copying, erase in disk
     }
-  } fputc('\n', swap_space); // End pages with newline for readability of file
+  }
   
   // Now transfer contents from file to disk/memory
-  start = to;
-  fseek(swap_space, file_start, SEEK_SET); // get back to where we began putting content into swap_space
-
-  int ch;
-  while ((ch = fgetc(swap_space)) != EOF && ch != '\n' && to < start + 16) {
+  printf("At position %d in file\n", fseek(swap_space, 0, SEEK_SET));
+  unsigned char ch;
+  for (int i = new_offset; (ch = fgetc(swap_space)) != '\n' && i < new_offset + 16; i++) {
     if (to_disk) {
-      if (to == start)
-	disk[to++] = (unsigned char) (ch - 4); // Set leftmost bit to 0 to mark that we are in disk
-      else
-	disk[to++] = (unsigned char) ch;
-    } else { // assume a page has been allocated for us already
-      if (to == start)
-	memory[to++] = (unsigned char) (ch + 4); // Set rightmost bit 1 to mark that we are in memory
-      else
-	memory[to++] = (unsigned char) (ch);
-    }
+      disk[i] = ch;
+    } else 
+      memory[i] = ch;
   }
 
-  // Check swap was successful (if not whole program is basically messed up and we must Ctrl-C)
-  if (ch == EOF) {
-    printf("ERROR: Somehow encountered end of file while swapping file index %d to %d\n", file_start, to);
-    return -1;
-  } else if (to != start + 16) {
-    printf("ERROR: Somehow went to %d from file from %d\n", to, file_start);
-    return -1;
-  }
+  // Update page table entry fields
+  if (to_disk) entry->io = 0;
+  else entry->io = 1;
+  
+  int old_pfn = entry->pfn;
+  entry->pfn = new_offset / 16;
 
-  // Update registers appropriately
-  if (to_disk) { 
-    if (is_page_table)
-      disk_pgtable_offsets[pid] = start;
-    else
-      disk_page_offsets[pid] = start;
+  // Update free lists appropriately
+  if (to_disk) {
+    disk_free[entry->pfn] = -1;
   } else {
-    if (is_page_table)
-      mem_pgtable_offsets[pid] = start;
-    else
-      mem_page_offsets[pid] = start;
+    free_list[entry->pfn] = -1;
   }
   
   // Adjust next_evict to follow round-robin eviction strategy
-  if (next_evict == 3) next_evict = 0;
+  if (next_evict == 3) next_evict = 1;
   else next_evict++;
   
   // Notify user and exit
   if (to_disk) {
-    printf("Swapped frame %d into disk at %d\n", start / 16, to - 16);
+    printf("Swapped frame %d into disk at %d\n", old_pfn, new_offset);
     return 0;
   } else {
-    printf("Swapped disk offset %d into frame %d\n", from - 16, start / 16);
+    printf("Swapped disk offset %d into frame %d\n", prev_offset, entry->pfn);
     return 0;
   }
 
--- readme.txt
+++ readme.txt
@@ -3,46 +3,41 @@ Sydney Fisher
 
 Part 1:
      Page table implementation:
-     	  Our page tables (one for each process) are each located within their designated page in memory. The indices of memory representing the starts of their pages are stored as
-	  integers in a global array named registers, as the stored indices are meant to simulate hardware registers. In memory itself they follow a pattern:
+     	  Our page table implementation made use of a byte-wide struct to represent the page table entries:
 
-	  Index of Memory:  [0/16/32/48] [+1] [+2] [+3] ... (Same pattern four times) ... [15/31/47/63] (End of page)
- 	  Content:            Disk/Mem   VPN  PFN  R/W  ...          ...                       R/W
+	  typedef struct pte {
+	  	  u_int8_t io:1;  // In disk or memory
+		  u_int8_t pid:2; // PID
+		  u_int8_t vpn:2; // Virtual page number
+		  u_int8_t pfn:2; // Physical frame number
+		  u_int8_t rw:1;  // Read or read/write
+	  } pte;
 
-	  That is, the start of the page (which will be a multiple of 16 since memory is 64 chars divided into 4 pages) holds the ID of the process whose maps it contains info about;
-	  The first index afterwards (Could be 1,17,etc.) holds the virtual page number of the page mapped, the second index holds physical frame number of the page in memory that was
-	  actually mapped to the process, and the third index holds the read/write value of the map. This pattern continues throughout the page.
+	  In order to store this struct inside a char in memory, we use memcpy() with a pointer to a pte struct and one to a char in memory. Then, to access a field of the struct from the char in
+	  memory, we simply cast a reference to it to one of type pte*, and access the field we want from the result:
+
+	  pte* entry = (pte*) &memory[0]  // or &disk[0], where 0 is part of a page known to contain the page table
+	  entry->pid; // process this page table entry refers to
+	  entry->io;  // whether page refered to by this page table entry is in disk or memory
+	  ...
 
      Tests:
-	0,map,0,1
-	Mapped virtual address 0 (page 0) to physical frame 1
+	Map:
+		0,map,0,1
+		Mapped virtual address 0 (page 0) to physical frame 1
 
-	0,map,15,1
-	Virtual address 15 has already been mapped to physical frame 1 for process 0
+		0,map,15,1
+		Virtual address 15 has already been mapped to physical frame 1 for process 0
 
-	1,map,62,0
-	Mapped virtual address 48 (page 3) to physical frame 3
+		1,map,62,0
+		Mapped virtual address 48 (page 3) to physical frame 3
 
-	1,store,63,12
-	Virtual page 3 (physical frame 3) for process 1 is not writeable
+		1,store,63,12
+		Virtual page 3 (physical frame 3) for process 1 is not writeable
 
-	2,map,17,1
-	Not enough pages left!
+		2,map,17,1
+		Not enough pages left!
 
-	0,map,32,1
-	Not enough pages left!
+Part 2:
 
-	0,load,0,NA
-	Nothing stored at virtual address 0 (physical address 1)
-
-	0,store,0,212
-	Stored value 212 at virtual address 0 (physical address 16)
-
-	0,load,1,NA
-	Nothing stored at virtual address 0 (physical address 1)
-
-	0,load,0,NA
-	The value 212 is virtual address 0 (physical address 16)
-	
-	  
-				
+     
